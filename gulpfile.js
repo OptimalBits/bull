e4b6262053d478bc2741fdd75fbe59fbab938073
Manuel Astudillo <manuel@optimalbits.com>
2015-03-15 21:17:51 +0100

    upgraded node_redis and added gulp and an eslint task.

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/*eslint-env node */
+'use strict';
+
+var gulp = require('gulp'),
+eslint = require('gulp-eslint');
+
+gulp.task('lint', function (){
+  // Note: To have the process exit with an error code (1) on
+  //  lint error, return the stream and pipe to failOnError last.
+  return gulp.src([
+    './lib/job.js',
+    './lib/queue.js',
+    '!./test/**'
+    ])
+    .pipe(eslint({
+      rules: {
+            'space-after-keywords': [2, 'never'],
+            indent: [2, 2],
+            'valid-jsdoc': 0,
+            'func-style': 0,
+            'no-use-before-define': 0,
+            camelcase: 1,
+            'no-unused-vars': 1,
+            'no-alert': 1,
+            'no-console': 1,
+            'quotes': [2, "single"],
+            'no-underscore-dangle': 0
+        },
+        globals: {
+          'define': true
+        }
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('default', ['lint'], function (){
+  // This will only run if the lint task is successful...
+});
