9b12e9e47059ae1acb600350c4cae73c5ba3cb13
Manuel Astudillo <manuel@optimalbits.com>
2017-05-21 11:37:58 +0200

    Check for lock before moving to delayed

--- lib/commands/moveToDelayed-3.lua
+++ lib/commands/moveToDelayed-3.lua
@@ -8,20 +8,28 @@
 
     ARGV[1] delayedTimestamp
     ARGV[2] the id of the job
+    ARGV[3] queue token
 
   Output:
     0 - OK
-    -1 - Missing job.
+   -1 - Missing job.
+   -2 - Job is locked.
 
   Events:
     - delayed key.
 ]]
 if redis.call("EXISTS", KEYS[3]) == 1 then
-  local score = tonumber(ARGV[1])
-  redis.call("ZADD", KEYS[2], score, ARGV[2])
-  redis.call("PUBLISH", KEYS[2], (score / 0x1000))
-  redis.call("LREM", KEYS[1], 0, ARGV[2])
-  return 0;
+  local lockKey = KEYS[3] .. ':lock'
+  local lock = redis.call("GET", lockKey)
+  if (not lock) or (lock == ARGV[3]) then 
+    local score = tonumber(ARGV[1])
+    redis.call("ZADD", KEYS[2], score, ARGV[2])
+    redis.call("PUBLISH", KEYS[2], (score / 0x1000))
+    redis.call("LREM", KEYS[1], 0, ARGV[2])
+    return 0;
+  else
+    return -2
+  end
 else
   return -1
 end
