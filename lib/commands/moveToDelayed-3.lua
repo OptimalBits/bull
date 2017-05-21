9e3d7841528e4444e5a580bf3cd59306eeb2d382
Manuel Astudillo <manuel@optimalbits.com>
2017-05-21 10:52:27 +0200

    removed dead code

--- lib/commands/moveToDelayed-3.lua
+++ lib/commands/moveToDelayed-3.lua
@@ -8,7 +8,6 @@
 
     ARGV[1] delayedTimestamp
     ARGV[2] the id of the job
-    ARGV[3] timestamp
 
   Output:
     0 - OK
@@ -19,12 +18,8 @@
 ]]
 if redis.call("EXISTS", KEYS[3]) == 1 then
   local score = tonumber(ARGV[1])
-  if score ~= 0 then
-    redis.call("ZADD", KEYS[2], score, ARGV[2])
-    redis.call("PUBLISH", KEYS[2], (score / 0x1000))
-  else
-    redis.call("ZADD", KEYS[2], ARGV[3], ARGV[2])
-  end
+  redis.call("ZADD", KEYS[2], score, ARGV[2])
+  redis.call("PUBLISH", KEYS[2], (score / 0x1000))
   redis.call("LREM", KEYS[1], 0, ARGV[2])
   return 0;
 else
