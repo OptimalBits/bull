c5745d4a11dc6236b7653fcefe8cfc83ee4e97f0
Manuel Astudillo <manuel@optimalbits.com>
2017-06-19 10:04:55 +0200

    added some worker methods

---
+++ test/test_worker.js
@@ -0,0 +1,34 @@
+/*eslint-env node */
+'use strict';
+
+var expect = require('chai').expect;
+var utils = require('./utils');
+var redis = require('ioredis');
+
+describe('workers', function () {
+  var queue;
+
+  beforeEach(function(){
+    var client = new redis();
+    return client.flushdb().then(function(){
+      queue = utils.buildQueue('repeat', {settings: { 
+        guardInterval: Number.MAX_VALUE,
+        stalledInterval: Number.MAX_VALUE
+      }});
+    });
+  });
+
+  afterEach(function(){
+    return queue.close();
+  });
+
+  it('should get all workers for this queue', function (done) {
+    queue.process(function(){
+    });
+
+    queue.getWorkers().then(function(workers){
+      console.error(workers);
+      done();
+    });
+  });
+});
