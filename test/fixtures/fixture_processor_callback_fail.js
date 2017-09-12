6022edd4490aa88945c4aeee78e22225db152709
Manuel Astudillo <manuel@optimalbits.com>
2017-09-12 08:51:05 +0200

    added missing fixtures

---
+++ test/fixtures/fixture_processor_callback_fail.js
@@ -0,0 +1,12 @@
+/**
+ * A processor file to be used in tests.
+ *
+ */
+
+var Promise = require('bluebird');
+
+module.exports = function(job, done){
+  Promise.delay(500).then(function(){
+    done(new Error('Manually failed processor'));
+  });
+};
