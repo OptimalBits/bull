4cbb7985c65cfcc3d746516716b56f0b504da76a
Manuel Astudillo <manuel@optimalbits.com>
2017-12-05 10:30:17 +0100

    Fixed #807

--- lib/commands/removeRepeatable-2.lua
+++ lib/commands/removeRepeatable-2.lua
@@ -7,13 +7,16 @@
 
     ARGV[1] repeat job id
     ARGV[2] repeat job key
+    ARGV[3] queue key
 ]]
 local millis = redis.call("ZSCORE", KEYS[1], ARGV[2])
 
 if(millis) then
   -- Delete next programmed job.
   local repeatJobId = ARGV[1] .. millis
-  redis.call("ZREM", KEYS[2], repeatJobId);
+  if(redis.call("ZREM", KEYS[2], repeatJobId) == 1) then
+    redis.call("DEL", ARGV[3] .. repeatJobId)
+  end
 end
 
 redis.call("ZREM", KEYS[1], ARGV[2]);
