abb4e7a6ba058b329400b2248cd04af4724e81a2
Juan-Cruz <juan.cruz@ibm.com>
2018-08-14 15:33:03 +0200

    Removing redis.call("DECR", KEYS[8]) in lua file
    Apparently, the sentence redis.call("DECR", KEYS[8]) is not needed (the tests pass), so we proceed to modify the files to exclude the 'id' param and remove that sentence.

--- lib/commands/removeJob-8.lua
+++ lib/commands/removeJob-8.lua
@@ -10,8 +10,7 @@
       KEYS[5] 'completed',
       KEYS[6] 'failed',
       KEYS[7] 'priority',
-      KEYS[8] 'id', 
-      KEYS[9] jobId
+      KEYS[8] jobId
 
       ARGV[1]  jobId
       ARGV[2]  lock token
@@ -22,7 +21,7 @@
 
 -- TODO PUBLISH global event 'removed'
 
-local lockKey = KEYS[9] .. ':lock'
+local lockKey = KEYS[8] .. ':lock'
 local lock = redis.call("GET", lockKey)
 if not lock then             -- or (lock == ARGV[2])) then
   redis.call("LREM", KEYS[1], 0, ARGV[1])
@@ -32,8 +31,7 @@ if not lock then             -- or (lock == ARGV[2])) then
   redis.call("ZREM", KEYS[5], ARGV[1])
   redis.call("ZREM", KEYS[6], ARGV[1])
   redis.call("ZREM", KEYS[7], ARGV[1])
-  redis.call("DECR", KEYS[8])
-  redis.call("DEL", KEYS[9])
+  redis.call("DEL", KEYS[8])
   return 1
 else
   return 0
