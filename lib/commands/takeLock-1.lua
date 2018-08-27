94df8c368fcc2cabfb078fdc96546273b828708d
Tomasz Rychlewicz <tomasz.rychlewicz@explaineverything.com>
2018-08-27 15:01:38 +0200

    1. Fix for call job.lockKey in scripts.js (it is function, but called as property) 2. Possibility to take lock without checking key existance

--- lib/commands/takeLock-1.lua
+++ lib/commands/takeLock-1.lua
@@ -3,13 +3,22 @@
 
      Input:
         KEYS[1] 'lock',
-      
+
         ARGV[1]  token
         ARGV[2]  lock duration in milliseconds
-      
+        ARGV[3]  force
+
       Output:
         "OK" if lock extented succesfully.
 ]]
+if ARGV[3] == "1" then
+    if redis.call("SET", KEYS[1], ARGV[1], "PX", ARGV[2]) then
+        return 1
+    else
+      return 0
+    end
+end
+
 if redis.call("SET", KEYS[1], ARGV[1], "NX", "PX", ARGV[2]) then
   return 1
 else
