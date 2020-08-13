450562f24b19aba583485b5e85436c8be9891e4b
Manuel Astudillo <manuel@optimalbits.com>
2020-08-13 11:59:06 +0200

    feat: make pause forward compatible with bullmq

--- lib/commands/pause-5.lua
+++ lib/commands/pause-5.lua
@@ -6,6 +6,7 @@
       KEYS[2] 'paused' or 'wait'
       KEYS[3] 'meta-paused'
       KEYS[4] 'paused' o 'resumed' event.
+      KEYS[5] 'meta' this key is only used in BullMQ and above.
 
       ARGV[1] 'paused' or 'resumed'
 
@@ -20,8 +21,15 @@ end
 
 if ARGV[1] == "paused" then
   rcall("SET", KEYS[3], 1)
+
+  -- for forwards compatibility
+  rcall("HSET", KEYS[5], "paused", 1)
 else
   rcall("DEL", KEYS[3])
+
+  -- for forwards compatibility
+  rcall("HDEL", KEYS[5], "paused")
+
 end
 
 rcall("PUBLISH", KEYS[4], ARGV[1])
