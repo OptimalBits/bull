d60d028e13a84245b0b07ffb008b3cc5ad55a8c1
Maxime Colmant <maxime.colmant@gmail.com>
2021-07-06 16:06:07 +0200

    chore: drop the NodeJS 10 support, support ESM sandboxed jobs

---
+++ test/fixtures/fixture_processor.cjs
@@ -0,0 +1,13 @@
+/**
+ * A processor file to be used in tests.
+ *
+ */
+'use strict';
+
+const delay = require('delay');
+
+module.exports = function(/*job*/) {
+  return delay(500).then(() => {
+    return 42;
+  });
+};
