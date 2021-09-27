c2194fead19e8d7a1159672e2f21de2a29d39990
Dobes Vandermeer <dobesv@gmail.com>
2021-09-26 20:37:02 -0700

    chore(commands): fix comment in takeLock

--- lib/commands/takeLock-1.lua
+++ lib/commands/takeLock-1.lua
@@ -8,7 +8,7 @@
         ARGV[2]  lock duration in milliseconds
       
       Output:
-        "OK" if lock extented succesfully.
+        "OK" if lock taken successfully.
 ]]
 if redis.call("SET", KEYS[1], ARGV[1], "NX", "PX", ARGV[2]) then
   return 1
